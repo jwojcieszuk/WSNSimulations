2501dd810899195034b80ae4a2e93851b666b124
jwojcieszuk <jwojcieszuk@gmail.com>
2022-03-26 12:30:23 -0400

    possiblity to define more than one scenario

--- run.py
+++ run.py
@@ -11,42 +11,46 @@ from metrics import RoutingAlgorithmMetrics
 
 
 def run():
-    logging.basicConfig(filename='simulator.log', filemode='w', encoding='utf-8', level=logging.INFO)
-    simulation_metrics = list()
-
     with open('scenario.json', 'r') as scenario_file:
         data = json.load(scenario_file)
-        env = Environment(num_of_nodes=data['num_of_nodes'])
 
-        # gathering simulation results
-        for algorithm in data['algorithms']:
-            if algorithm in cfg.supported_algorithms:
-                logging.info(f'Starting simulation for {algorithm}')
-                metric: RoutingAlgorithmMetrics = env.simulate(cfg.supported_algorithms[algorithm])
-                simulation_metrics.append(metric)
+        for scenario in data.keys():
+            run_scenario(data[scenario])
 
-        if not simulation_metrics:
-            logging.info("Couldn't find any supported routing algorithms defined in scenario")
-            return
 
-        # plotting metrics
-        for scenario_metric in data['metrics']:
-            if scenario_metric in cfg.supported_metrics:
-                color = iter(cm.rainbow(np.linspace(0, 1, len(simulation_metrics))))
+def run_scenario(scenario):
+    logging.basicConfig(filename=f'scenario_{scenario}.log', filemode='w', encoding='utf-8', level=logging.INFO)
+    simulation_metrics = list()
+    env = Environment(num_of_nodes=scenario['num_of_nodes'])
 
-                for simulation_metric in simulation_metrics:
-                    metric = simulation_metric.__getattribute__(scenario_metric)
-                    plt.plot(
-                        simulation_metric.rounds_num,
-                        metric,
-                        c=next(color),
-                        label=simulation_metric.algorithm_name
-                    )
+    for algorithm in scenario['algorithms']:
+        if algorithm in cfg.supported_algorithms:
+            logging.info(f'Starting simulation for {algorithm}')
+            metric: RoutingAlgorithmMetrics = env.simulate(cfg.supported_algorithms[algorithm])
+            simulation_metrics.append(metric)
 
-                plt.ylabel(scenario_metric)
-                plt.xlabel('Rounds')
-                plt.legend(loc="lower left")
-                plt.show()
+    if not simulation_metrics:
+        logging.info("Couldn't find any supported routing algorithms defined in scenario")
+        return
+
+    # plotting metrics
+    for scenario_metric in scenario['metrics']:
+        if scenario_metric in cfg.supported_metrics:
+            color = iter(cm.rainbow(np.linspace(0, 1, len(simulation_metrics))))
+
+            for simulation_metric in simulation_metrics:
+                metric = simulation_metric.__getattribute__(scenario_metric)
+                plt.plot(
+                    simulation_metric.rounds_num,
+                    metric,
+                    c=next(color),
+                    label=simulation_metric.algorithm_name
+                )
+
+            plt.ylabel(scenario_metric)
+            plt.xlabel('Rounds')
+            plt.legend(loc="lower left")
+            plt.show()
 
 
 if __name__ == "__main__":
